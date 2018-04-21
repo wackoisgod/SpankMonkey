b1676e02f1ccd31573011a29ef8d86afac9774f0
Andrew Spiering <aspiering@gmail.com>
2018-04-21 15:22:17 -0700

    adding Enumerations class
    Adding a game wide enumerations class

---
+++ Assets/Scripts/Common/Data/Game/Enumerations.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Enumerations
+{
+	public enum OrbColor
+	{
+		empty,
+		dark,
+		fire,
+		light,
+		water,
+		wood,
+		attack,
+		length
+	}
+
+	public enum Player
+	{
+		PlayerOne,
+		PlayerNone
+	}
+
+	public enum DragTargets
+	{
+		Board,
+		Card,
+		Orb,
+		Battle,
+		Enemy,
+		None
+	}
+}
